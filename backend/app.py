c7bedac7c90da95d7b7181f44bfac25f7034849b
ZainUlabideen460 <m.zain.ul.abideen.460@gmail.com>
2025-05-05 14:30:58 -0700

    update  main port

--- backend/app.py
+++ backend/app.py
@@ -20,4 +20,4 @@ app.register_blueprint(api, url_prefix='/api')
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
