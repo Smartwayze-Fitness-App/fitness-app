04a0c7de14b1dcc6a6984a23326748f6ecf0c82b
ZainUlabideen460 <m.zain.ul.abideen.460@gmail.com>
2025-05-09 20:19:04 -0700

    updatd full code

--- backend/config.py
+++ backend/config.py
@@ -1,7 +1,7 @@
 import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-
+# jjijij
 class Config:
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:21arid483fyp@localhost:3306/dbfitnessapp'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
